7759ccf8623763b95012cf7926039f05bf6f0032
Ondrej Sika <ondrej@ondrejsika.com>
2020-05-01 19:25:36 +0200

    gitlab_clusters - remove dev cluser data

--- gitlab_clusters/data.tf
+++ gitlab_clusters/data.tf
@@ -1,9 +1,5 @@
 variable "faker_app_project_id" {}
 
-data "gitlab_project" "faker-app" {
-  id = var.faker_app_project_id
-}
-
 data "gitlab_group" "faker" {
   full_path = "faker"
 }
@@ -12,6 +8,3 @@ data "rancher2_cluster" "prod" {
   name = "prod"
 }
 
-data "rancher2_cluster" "dev" {
-  name = "dev"
-}
